5ef7012a7fdd82a422c276f5faf4a3440ec152b8
Alex <AlexGrimes.se@gmail.com>
2023-01-03 19:43:55 -0500

    non concurrent scanner

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		fmt.Println(address)
 
-	if err == nil {
-		fmt.Println("Connection successful")
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		fmt.Printf("%d open\n", i)
 	}
 }
